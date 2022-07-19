109a14732d9c646ca5dc3d6fc14f4696b4b31e22
Jason Young <jasonyeong790@gmail.com>
2022-07-20 08:59:33 +0900

    Redis Init할 때 시스템 로그

--- src/main/java/com/gnm/adrunner/server/Application.java
+++ src/main/java/com/gnm/adrunner/server/Application.java
@@ -1,5 +1,7 @@
 package com.gnm.adrunner.server;
 
+import java.util.List;
+
 import com.gnm.adrunner.config.GlobalConstant;
 import com.gnm.adrunner.config.RedisConfig;
 import com.gnm.adrunner.server.repo.ServerInstanceRepository;
@@ -31,19 +33,33 @@ public class Application {
 	public void doSomethingAfterStartup() {
 
 		// 관리자 서버 호스트 조회
+		System.out.println("######################################################################");
 		GlobalConstant.SERVER_HOST_ADMIN = serverInstanceRepository.getServerHost(GlobalConstant.SERVER_TYPE_ADMIN);
+		System.out.println("SERVER HOST(ADMIN) : " 		+ GlobalConstant.SERVER_HOST_ADMIN);
 
 		// 클릭 서버 호스트 조회
 		GlobalConstant.SERVER_HOST_CLICK = serverInstanceRepository.getServerHost(GlobalConstant.SERVER_TYPE_CLICK);
+		System.out.println("SERVER HOST(CLICK)) : " 	+ GlobalConstant.SERVER_HOST_CLICK);
 
 		// 포스트백 서버 호스트 조회
 		GlobalConstant.SERVER_HOST_PBACK = serverInstanceRepository.getServerHost(GlobalConstant.SERVER_TYPE_PBACK);
+		System.out.println("SERVER HOST(POSTBACK) : " 	+ GlobalConstant.SERVER_HOST_PBACK);
 
 	 
 		// Redis 그룹 개수 조회
 		GlobalConstant.NUMBER_OF_REDIS_GROUP = systemConfigRepository.findNumberOfRedisGroup();
-		for(int i=0; i<GlobalConstant.NUMBER_OF_REDIS_GROUP; i++)
-			GlobalConstant.SERVER_HOST_REDIS.add(serverInstanceRepository.getServerClientIpWithGroup(GlobalConstant.SERVER_TYPE_REDIS, i));
+		System.out.println("NUMBER OF REDIS GROUP : " 	+ GlobalConstant.NUMBER_OF_REDIS_GROUP);
+		for(int i=0; i<GlobalConstant.NUMBER_OF_REDIS_GROUP; i++){
+			List<String> REDIS_CLIENT_IP = serverInstanceRepository.getServerClientIpWithGroup(GlobalConstant.SERVER_TYPE_REDIS, i);
+			GlobalConstant.SERVER_HOST_REDIS.add(REDIS_CLIENT_IP);
+			System.out.print("GROUP NUMBER : #"+i+" - ");
+			for(String e : REDIS_CLIENT_IP)System.out.print("["+e+"]");
+			System.out.println();
+			REDIS_CLIENT_IP = null;
+		}
+
+		System.out.println("######################################################################");
+		System.out.println();
 	
 
 		// Redis 시작
